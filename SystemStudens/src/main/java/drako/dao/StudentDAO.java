702dce33f1f92746ad5ab2cd8e9a9b89972e5bca
drako9159 <chochito9159@gmail.com>
2023-10-12 20:27:46 -0600

    complete crud in students

--- SystemStudens/src/main/java/drako/dao/StudentDAO.java
+++ SystemStudens/src/main/java/drako/dao/StudentDAO.java
@@ -93,20 +93,93 @@ public class StudentDAO {
         return false;
     }
 
+    public boolean modifyStudent(Student student) {
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "update student set name=?, last_name=?, phone=?, email=? where id_student = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, student.getName());
+            ps.setString(2, student.getLastName());
+            ps.setString(3, student.getPhone());
+            ps.setString(4, student.getEmail());
+            ps.setInt(5, student.getIdStudent());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error in modify student: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error in: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteStudent(Student student) {
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "delete from student where id_student = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, student.getIdStudent());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error in delete student: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error in: " + e.getMessage());
+            }
+        }
+        return false;
+
+    }
+
     public static void main(String[] args) {
         var studentDAO = new StudentDAO();
+
+        var studentDelete = new Student(2);
+        var deleted = studentDAO.deleteStudent(studentDelete);
+        if (deleted) {
+            System.out.println("Student is deleted: " + studentDelete);
+        } else {
+            System.out.println("Student is not deleted: " + studentDelete);
+        }
+
+
+        //var studentModify = new Student(1, "Antonio", "Jaramillo", "5532317335", "drakolin@gmail.com");
+        //var modify = studentDAO.modifyStudent(studentModify);
+        //if (modify) {
+        //    System.out.println("Student is changed: " + studentModify);
+        //} else {
+        //    System.out.println("Student not changed: " + studentModify);
+        //}
+
+        //var newStudent = new Student("Rogelio", "Lara", "5532317898", "rogelio@gmail.com");
+        //var adder = studentDAO.addStudent(newStudent);
+        //if (adder) {
+        //    System.out.println("Student added: " + newStudent);
+        //} else {
+        //    System.out.println("Not added: " + newStudent);
+        //}
+
         System.out.println("List of students: ");
         List<Student> students = studentDAO.listStudents();
-        //students.forEach(System.out::println);
+        students.forEach(System.out::println);
 
-        var student1 = new Student(1);
-        System.out.println("Student before search: " + student1);
-        var studentFind = studentDAO.findStudentById(student1);
-        if (studentFind) {
-            System.out.println("Search result: " + student1);
-        } else {
-            System.out.println("Student not found: " + student1.getIdStudent());
-        }
+        //var student1 = new Student(1);
+        //System.out.println("Student before search: " + student1);
+        //var studentFind = studentDAO.findStudentById(student1);
+        //if (studentFind) {
+        //    System.out.println("Search result: " + student1);
+        //} else {
+        //    System.out.println("Student not found: " + student1.getIdStudent());
+        //}
 
 
     }
