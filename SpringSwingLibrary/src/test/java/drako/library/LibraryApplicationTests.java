b97bc755e74cc65ac6e818271d42e77642c3a427
drako9159 <chochito9159@gmail.com>
2023-10-14 16:29:58 -0600

    add new project for swing and sprign combination

---
+++ SpringSwingLibrary/src/test/java/drako/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package drako.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
